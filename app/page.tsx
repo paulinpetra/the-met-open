2ab0feec933d13821e10dc340d761f246aad177d
Petra Paulin <petra.oster.paulin@gmail.com>
2026-01-27 14:03:11 +0100

    add hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,33 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Welcome to the main page</main>;
+  return (
+    <main>
+      <section className="relative h-96 w-full">
+        <Image
+          src="/the-met-hero.jpg"
+          alt="The Met Museum Entrance"
+          fill //stretches the image to fill the parent container(replaces w and h)
+          className="object-cover"
+          priority //load this first
+        />
+
+        {/* Overlay on image */}
+        <div className="absolute inset-0 bg-black/60" />
+        {/* Text layer */}
+        <div className="relative z-10 container mx-auto px-4 h-full flex flex-col justify-center">
+          <h1 className="text-5xl leading-tight font-bold text-white max-w-3xl">
+            MetOpen — Your Gateway to The Met’s Masterpieces
+          </h1>
+          <p className="mt-4 text-lg text-neutral-200 max-w-2xl">
+            Browse artworks that have shaped cultures across continents. Our
+            interactive online exhibition brings The Met’s most compelling works
+            to your screen — with rich imagery, detailed metadata, and smart
+            search built on The Met Collection API.
+          </p>
+        </div>
+      </section>
+      <section>cards...</section>
+    </main>
+  );
 }
--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,32 +1,37 @@
 import Link from "next/link";
+//all caps common for constants
+const navigationLinks = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
 
 export default function Navigation() {
   return (
-    <nav className="border-b sticky top-0 bg-neutral-100/80 backdrop-blur-xs dark:bg-neutral-800/80">
-      <div className="font-bold container mx-auto px-4 py-4 flex items-center justify-between">
-        <Link href="/"></Link>
+    <nav className="sticky top-0 border-b bg-neutral-100/80 backdrop-blur-xs dark:bg-neutral-800/80">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4 font-bold">
+        {/* logo */}
+        <div className="flex flex-col">
+          <Link
+            href="/"
+            className="text-2xl font-serif tracking-wide text-amber-900 dark:text-amber-400"
+          >
+            MetOpen
+          </Link>
+          <span className="block w-12 h-0.5 bg-amber-600 mt-0.5 dark:bg-amber-400" />
+        </div>
+
         <ul className="flex gap-6">
-          <li>
-            <Link className="hover:text-sky-400 transition-colors" href="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-sky-400 transition-colors"
-              href="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="hover:text-sky-400 transition-colors"
-              href="/contact"
-            >
-              Contact
-            </Link>
-          </li>
+          {navigationLinks.map(({ name, href }) => (
+            <li key={href}>
+              <Link
+                href={href}
+                className="transition-colors hover:text-teal-400"
+              >
+                {name}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </nav>

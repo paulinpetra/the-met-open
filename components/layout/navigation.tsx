1161c6726ab82f25502212b7e82297038313398a
Petra Paulin <petra.oster.paulin@gmail.com>
2026-03-09 14:44:50 +0100

    change header text

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -12,6 +12,10 @@ export default function Navigation() {
       <div className="container mx-auto flex items-center justify-between px-4 py-4 font-bold">
         {/* logo */}
         <div className="flex flex-col">
+          <span className="text-xs uppercase tracking-widest text-neutral-500 dark:text-neutral-400">
+            Welcome to
+          </span>
+
           <Link
             href="/"
             className="text-2xl font-serif tracking-wide text-amber-900 dark:text-amber-400"
